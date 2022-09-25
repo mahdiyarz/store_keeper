71158e31b45c4d8a78e8552873646afeccf6f114
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-09-25 12:04:58 +0330

    Create Home screen and first presentation

---
+++ lib/screens/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/arrival-goods');
+                },
+                icon: const Icon(Icons.store_mall_directory_rounded),
+                label: const Text('شمارش ورودی انبار')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.storage_rounded),
+                label: const Text('انبار گردانی')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.refresh_rounded),
+                label: const Text('بروزرسانی اطلاعات')),
+          ],
+        ),
+      ),
+    );
+  }
+}
