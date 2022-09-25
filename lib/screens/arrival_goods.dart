c73e80286a3074b17301235a07283f4806e80921
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-09-25 12:04:06 +0330

    Create ArrivalGoods screen

---
+++ lib/screens/arrival_goods.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ArrivalGoods extends StatelessWidget {
+  static const routeName = '/arrival-goods';
+  const ArrivalGoods({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
