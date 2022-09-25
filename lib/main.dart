8a2e8e13a516cb3fa983c274671bfb7561cc9e98
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-09-25 12:05:31 +0330

    Add Home screen on Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../screens/arrival_goods.dart';
+import '../screens/home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: const Home(),
+      routes: {
+        '/arrival-goods': (context) => const ArrivalGoods(),
+      },
+    );
   }
 }
