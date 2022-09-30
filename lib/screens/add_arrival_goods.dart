ccb5aaad91e05143d38dfc1f6c294c578e1ff93f
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-09-30 18:58:16 +0330

    Add screen style on arrival goods screens

--- lib/screens/add_arrival_goods.dart
+++ lib/screens/add_arrival_goods.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/screens_style.dart';
+
 class AddArrivalGoods extends StatelessWidget {
   static const routeName = '/add-arrival-goods';
   const AddArrivalGoods({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double _width = MediaQuery.of(context).size.width;
+
+    return ScreensStyle(
+      screenTitle: 'ثبت ورودی انبار',
+      screenDescription: 'لطفا تمامی قسمت های مربوطه را تکمیل کنید',
+      screenWidget: Center(child: Text('data')),
+      bottomWidget: ElevatedButton.icon(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        icon: const Icon(Icons.save),
+        label: const Text('ذخیره لیست'),
+        style: ElevatedButton.styleFrom(
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        ),
+      ),
+    );
   }
 }
--- lib/screens/arrival_goods.dart
+++ lib/screens/arrival_goods.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:store_keeper/widgets/screens_style.dart';
+
+import '../providers/arrival_goods_provider.dart';
 
 class ArrivalGoods extends StatelessWidget {
   static const routeName = '/arrival-goods';
@@ -6,6 +10,42 @@ class ArrivalGoods extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScreensStyle(
+      screenTitle: 'ورودی های انبار',
+      screenDescription: 'ثبت، اصلاح و تهیه گزارش ورودی های انبار',
+      screenWidget: FutureBuilder(
+        future: Provider.of<ArrivalGoodsProvider>(context, listen: false)
+            .fetchData(),
+        builder: (context, snapShots) =>
+            snapShots.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<ArrivalGoodsProvider>(
+                    builder: ((context, value, child) {
+                      return value.arrivalGoodsItems.isEmpty
+                          ? child as Widget
+                          : ListView.builder(
+                              itemBuilder: (context, index) => Text(value
+                                  .arrivalGoodsItems[index].arrivalGoodsDate
+                                  .toString()),
+                              itemCount: value.arrivalGoodsItems.length,
+                            );
+                    }),
+                    child: const Center(
+                      child: Text('هنوز هیچ لیست کالایی ثبت نشده است'),
+                    )),
+      ),
+      bottomWidget: ElevatedButton.icon(
+        onPressed: () {
+          Navigator.of(context).pushNamed('add-arrival-goods');
+        },
+        icon: const Icon(Icons.post_add_rounded),
+        label: const Text('لیست جدید'),
+        style: ElevatedButton.styleFrom(
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        ),
+      ),
+    );
   }
 }
