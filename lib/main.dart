13d0f14cf6f9dc2ac8533d0c9aa4fafd19425c4e
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-02 20:18:38 +0330

    Add theme color on main

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,15 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         theme: ThemeData(
           fontFamily: 'vazir',
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color.fromARGB(255, 120, 151, 171),
+            onPrimary: const Color.fromARGB(255, 255, 255, 255),
+            secondary: const Color.fromARGB(255, 216, 133, 163),
+            onSecondary: const Color.fromARGB(255, 255, 255, 255),
+            error: const Color.fromARGB(255, 164, 74, 63),
+            onError: const Color.fromARGB(255, 255, 255, 255),
+            primaryContainer: const Color.fromARGB(255, 253, 206, 185),
+          ),
         ),
         home: const Directionality(
           textDirection: TextDirection.rtl,
