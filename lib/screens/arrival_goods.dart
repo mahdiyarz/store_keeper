a2a728d398dfc6ca45060d51c3902ed87e19ba44
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-03 20:03:47 +0330

    set a add button on dialog show
    can push to brand screen, add new brand and back

--- lib/screens/arrival_goods.dart
+++ lib/screens/arrival_goods.dart
@@ -19,149 +19,12 @@ class ArrivalGoods extends StatefulWidget {
 
 class _ArrivalGoodsState extends State<ArrivalGoods> {
   var boxNumberController = TextEditingController();
-
   var brandNameController = TextEditingController();
-
-  void _showModalBottomSheet(
-      BuildContext context, double width, List<BrandsModel> brands) {
-    var showBrandName;
-    showModalBottomSheet(
-        isDismissible: false,
-        context: context,
-        builder: (context) {
-          return Directionality(
-            textDirection: TextDirection.rtl,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: ListView(
-                children: [
-                  const Text(
-                    'لطفا تعداد جعبه های ورودی و برند محصولات را وارد کنید و دکمه ثبت را بزنید...',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black45,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextFormField(
-                    controller: boxNumberController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      hintText: 'تعداد جعبه',
-                      hintStyle: TextStyle(color: Colors.black26),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'تعداد جعبه های ورودی را وارد نکردید!';
-                      }
-                      return null;
-                    },
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      backgroundColor: Theme.of(context)
-                          .colorScheme
-                          .secondary
-                          .withOpacity(.3),
-                    ),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => Directionality(
-                          textDirection: TextDirection.rtl,
-                          child: SimpleDialog(
-                            title: const Text('برند خود را انتخاب کنید'),
-                            children: [
-                              SizedBox(
-                                width: width * .5,
-                                height: width * 2,
-                                child: ListView.builder(
-                                  itemCount: brands.length,
-                                  itemBuilder: (context, index) => Center(
-                                    child: ListTile(
-                                      title: Text(brands[index].brandName),
-                                      onTap: () => Navigator.pop(context, [
-                                        brands[index].brandId,
-                                        brands[index].brandName,
-                                      ]),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ).then((returnValue) {
-                        if (returnValue != null) {
-                          brandNameController.text = returnValue.toString()[
-                              1]; //? Pass brand id to out controller
-                          setState(() {
-                            showBrandName =
-                                returnValue; //? To show brand name in real time on button label
-                          });
-                        }
-                      });
-                    },
-                    child: showBrandName == null
-                        ? const Text(
-                            'هنوز برند انتخاب نشده',
-                            style: TextStyle(
-                              color: Colors.black54,
-                            ),
-                          )
-                        : Text(
-                            showBrandName[1],
-                            style: const TextStyle(
-                              color: Colors.black54,
-                            ),
-                          ),
-                  ),
-                  const SizedBox(height: 25),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: brandNameController.text.isNotEmpty &&
-                                  boxNumberController.text.isNotEmpty
-                              ? () {
-                                  Navigator.of(context).pop();
-
-                                  Provider.of<ArrivalGoodsProvider>(context,
-                                          listen: false)
-                                      .insertData(ArrivalGoodsModel(
-                                          brandId: int.parse(
-                                              brandNameController.text),
-                                          numOfBoxes: int.parse(
-                                              boxNumberController.text),
-                                          arrivalGoodsDate: DateTime.now()));
-                                }
-                              : null,
-                          child: const Text('ثبت'),
-                        ),
-                      ),
-                      const SizedBox(width: 5),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('انصراف'),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          );
-        });
-  }
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     Provider.of<BrandsProvider>(context, listen: false).fetchData();
-    List<BrandsModel> brands =
-        Provider.of<BrandsProvider>(context, listen: false).brandsItems;
 
     return ScreensStyle(
       screenTitle: 'ورودی های انبار',
@@ -302,7 +165,7 @@ class _ArrivalGoodsState extends State<ArrivalGoods> {
       ),
       bottomWidget: ElevatedButton.icon(
         onPressed: () {
-          _showModalBottomSheet(context, width, brands);
+          _showModalBottomSheet(context, width);
           brandNameController.text = '';
           boxNumberController.text = '';
         },
@@ -313,5 +176,182 @@ class _ArrivalGoodsState extends State<ArrivalGoods> {
         ),
       ),
     );
+  }
+
+  void _showModalBottomSheet(BuildContext context, double width) {
+    var showBrandName;
+    var brandPostBack;
+    showModalBottomSheet(
+        isDismissible: false,
+        context: context,
+        builder: (context) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: ListView(
+                children: [
+                  const Text(
+                    'لطفا تعداد جعبه های ورودی و برند محصولات را وارد کنید و دکمه ثبت را بزنید...',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextFormField(
+                    controller: boxNumberController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: 'تعداد جعبه',
+                      hintStyle: TextStyle(color: Colors.black26),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'تعداد جعبه های ورودی را وارد نکردید!';
+                      }
+                      return null;
+                    },
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      backgroundColor: Theme.of(context)
+                          .colorScheme
+                          .secondary
+                          .withOpacity(.3),
+                    ),
+                    onPressed: () {
+                      List<BrandsModel> brands =
+                          Provider.of<BrandsProvider>(context, listen: false)
+                              .brandsItems;
+                      showDialog(
+                        context: context,
+                        builder: (context) => Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: SimpleDialog(
+                            title: const Text('برند خود را انتخاب کنید',
+                                textAlign: TextAlign.center),
+                            children: [
+                              Container(
+                                width: width * .5,
+                                height: width * .8,
+                                padding: const EdgeInsets.all(8),
+                                child: GridView.builder(
+                                  gridDelegate:
+                                      SliverGridDelegateWithMaxCrossAxisExtent(
+                                    maxCrossAxisExtent: width * .3,
+                                    childAspectRatio: 1.2,
+                                    crossAxisSpacing: 8,
+                                    mainAxisSpacing: 8,
+                                  ),
+                                  itemCount: brands.length,
+                                  itemBuilder: (context, index) => Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(15),
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary
+                                            .withOpacity(.3)),
+                                    child: Center(
+                                      child: ListTile(
+                                        title: FittedBox(
+                                          fit: BoxFit.none,
+                                          child: Text(brands[index].brandName,
+                                              style: const TextStyle(
+                                                  color: Colors.black54),
+                                              textAlign: TextAlign.center),
+                                        ),
+                                        onTap: () => Navigator.pop(context, [
+                                          brands[index].brandId,
+                                          brands[index].brandName,
+                                        ]),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                child: ElevatedButton.icon(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushReplacementNamed('/brands');
+                                  },
+                                  icon: const Icon(Icons.dangerous, size: 18),
+                                  label: const Text('برند مورد نظر موجود نیست'),
+                                  style: ElevatedButton.styleFrom(
+                                    tapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ).then((returnValue) {
+                        if (returnValue != null) {
+                          brandPostBack = returnValue;
+                          brandNameController.text = brandPostBack[0]
+                              .toString(); //? Pass brand id to out controller
+                          setState(() {
+                            showBrandName =
+                                returnValue; //? To show brand name in real time on button label
+                          });
+                        }
+                      });
+                    },
+                    child: showBrandName == null
+                        ? const Text(
+                            'هنوز برند انتخاب نشده',
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),
+                          )
+                        : Text(
+                            showBrandName[1],
+                            style: const TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                  ),
+                  const SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: brandNameController.text.isNotEmpty &&
+                                  boxNumberController.text.isNotEmpty
+                              ? () {
+                                  Navigator.of(context).pop();
+
+                                  Provider.of<ArrivalGoodsProvider>(context,
+                                          listen: false)
+                                      .insertData(ArrivalGoodsModel(
+                                          brandId: int.parse(
+                                              brandNameController.text),
+                                          numOfBoxes: int.parse(
+                                              boxNumberController.text),
+                                          arrivalGoodsDate: DateTime.now()));
+                                }
+                              : null,
+                          child: const Text('ثبت'),
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('انصراف'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
