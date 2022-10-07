34b0312332f60ddbcc152ab15b2db5073f08feb9
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-07 06:15:06 +0330

    Create my app file

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../providers/brands_provider.dart';
-import '../providers/arrival_goods_provider.dart';
-
-import '../screens/arrival_goods.dart';
-import '../screens/home.dart';
-import '../screens/brands.dart';
+import '../app/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => ArrivalGoodsProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => BrandsProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        theme: ThemeData(
-          fontFamily: 'vazir',
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color.fromARGB(255, 120, 151, 171),
-            onPrimary: const Color.fromARGB(255, 255, 255, 255),
-            secondary: const Color.fromARGB(255, 216, 133, 163),
-            onSecondary: const Color.fromARGB(255, 255, 255, 255),
-            error: const Color.fromARGB(255, 164, 74, 63),
-            onError: const Color.fromARGB(255, 255, 255, 255),
-            primaryContainer: const Color.fromARGB(255, 253, 206, 185),
-          ),
-        ),
-        home: const Directionality(
-          textDirection: TextDirection.rtl,
-          child: Home(),
-        ),
-        routes: {
-          '/arrival-goods': (context) => ArrivalGoods(),
-          '/brands': (context) => Brands(),
-        },
-      ),
-    );
-  }
 }
