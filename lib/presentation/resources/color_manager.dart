96243b372bad1ecdf9c21e59ff603d5eef69c423
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-17 14:25:27 +0330

    Update color_manager.dart

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static Color primary = HexColor.fromHex('#ED9728');
-  static Color darkPrimary = HexColor.fromHex('#d17d11');
-  static Color darkGrey = HexColor.fromHex('#525252');
+  static Color primary = HexColor.fromHex('#7897ab');
+  static Color darkPrimary = HexColor.fromHex('#9f97ab');
+  static Color primaryOpacity70 = HexColor.fromHex('#B37897ab');
+  static Color primaryContainer = HexColor.fromHex('#fdceb9');
+  static Color secondary = HexColor.fromHex('#d885a3');
+  static Color secondaryOpacity70 = HexColor.fromHex('#B3d885a3');
+  static Color error = HexColor.fromHex('#a44a3f');
   static Color grey = HexColor.fromHex('#737477');
   static Color grey1 = HexColor.fromHex('#707070');
   static Color grey2 = HexColor.fromHex('#797979');
-  static Color white = HexColor.fromHex('#FFFFFF');
-  static Color error = HexColor.fromHex('#e61f34');
   static Color lightGrey = HexColor.fromHex('#9E9E9E');
-  static Color primaryOpacity70 = HexColor.fromHex('#B3ED9728');
+  static Color darkGrey = HexColor.fromHex('#525252');
+  static Color white = HexColor.fromHex('#FFFFFF');
 }
 
 extension HexColor on Color {
