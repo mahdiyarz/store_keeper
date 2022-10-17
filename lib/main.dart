7c91ae10cedb82980983a163528ac5997fe48a2e
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-17 14:13:31 +0330

    implement error screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../presentation/resources/font_manager.dart';
+import '../presentation/resources/color_manager.dart';
 import '../app/my_app.dart';
 
 void main() {
+  ErrorWidget.builder = (FlutterErrorDetails details) => Material(
+        color: ColorManager.error,
+        child: Center(
+          child: Text(
+            details.exception.toString(),
+            style: TextStyle(
+              color: ColorManager.white,
+              fontSize: 18,
+              fontWeight: FontWeightManager.bold,
+            ),
+          ),
+        ),
+      );
   runApp(const MyApp());
 }
--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../resources/strings_manager.dart';
-import '../home/home.dart';
-import '../login/login.dart';
-import '../on_boarding/on_boarding.dart';
-import '../register/register.dart';
-import '../splash/splash.dart';
-import '../store_details/store_details.dart';
-import '../forgot_password/forgot_password.dart';
+// import '../login/login.dart';
+// import '../on_boarding/on_boarding.dart';
+// import '../register/register.dart';
+// import '../splash/splash.dart';
+// import '../store_details/store_details.dart';
+// import '../forgot_password/forgot_password.dart';
 
 class Routes {
   static const String splashRoute = '/';
@@ -22,20 +21,20 @@ class Routes {
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case Routes.splashRoute:
-        return MaterialPageRoute(builder: (_) => const SplashView());
-      case Routes.loginRoute:
-        return MaterialPageRoute(builder: (_) => const LoginView());
-      case Routes.onBoardingRoute:
-        return MaterialPageRoute(builder: (_) => const OnBoardingView());
-      case Routes.homeRoute:
-        return MaterialPageRoute(builder: (_) => const Home());
-      case Routes.registerRoute:
-        return MaterialPageRoute(builder: (_) => const RegisterView());
-      case Routes.forgotPasswordRoute:
-        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
-      case Routes.storeDetailsRoute:
-        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      // case Routes.splashRoute:
+      //   return MaterialPageRoute(builder: (_) => const SplashView());
+      // case Routes.loginRoute:
+      //   return MaterialPageRoute(builder: (_) => const LoginView());
+      // case Routes.onBoardingRoute:
+      //   return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      // case Routes.homeRoute:
+      //   return MaterialPageRoute(builder: (_) => const Home());
+      // case Routes.registerRoute:
+      //   return MaterialPageRoute(builder: (_) => const RegisterView());
+      // case Routes.forgotPasswordRoute:
+      //   return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      // case Routes.storeDetailsRoute:
+      //   return MaterialPageRoute(builder: (_) => const StoreDetailsView());
       default:
         return unDefinedRoute();
     }
