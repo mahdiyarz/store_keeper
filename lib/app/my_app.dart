0fba62300733a1f9bd0c9255e9fb5a2eac7e56cd
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-19 13:24:40 +0330

    implement route generator

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -4,9 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/brands_provider.dart';
 import '../providers/arrival_goods_provider.dart';
 
-import '../presentation/arrival_goods/arrival_goods.dart';
-import '../presentation/home/home.dart';
-import '../presentation/brands/brands.dart';
+import '../presentation/resources/routes_manager.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -27,27 +25,10 @@ class _MyAppState extends State<MyApp> {
           create: (context) => BrandsProvider(),
         ),
       ],
-      child: MaterialApp(
-        theme: ThemeData(
-          fontFamily: 'vazir',
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color.fromARGB(255, 120, 151, 171),
-            onPrimary: const Color.fromARGB(255, 255, 255, 255),
-            secondary: const Color.fromARGB(255, 216, 133, 163),
-            onSecondary: const Color.fromARGB(255, 255, 255, 255),
-            error: const Color.fromARGB(255, 164, 74, 63),
-            onError: const Color.fromARGB(255, 255, 255, 255),
-            primaryContainer: const Color.fromARGB(255, 253, 206, 185),
-          ),
-        ),
-        home: const Directionality(
-          textDirection: TextDirection.rtl,
-          child: Home(),
-        ),
-        routes: {
-          '/arrival-goods': (context) => const ArrivalGoods(),
-          '/brands': (context) => const Brands(),
-        },
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: RouteGenerator.getRoute,
+        initialRoute: Routes.homeRoute,
       ),
     );
   }
