ebdf008733c2f44960f32904a869342a3c4e0201
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-12-30 12:44:01 +0330

    Create import_models.dart

---
+++ lib/models/import_models.dart
@@ -0,0 +1,5 @@
+export '../models/arrival_goods_model.dart';
+export '../models/brands_model.dart';
+export '../models/count_goods_model.dart';
+export '../models/goods_model.dart';
+export '../models/laking_model.dart';
