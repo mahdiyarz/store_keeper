66af3eda1db2c07bd17e793594b84531281752f6
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-06 01:15:31 +0330

    Update values_manager.dart

--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -8,12 +8,16 @@ class AppMargin {
 }
 
 class AppPadding {
+  static const double p0 = 0.0;
+  static const double p5 = 5.0;
   static const double p8 = 8.0;
   static const double p12 = 12.0;
   static const double p14 = 14.0;
   static const double p16 = 16.0;
   static const double p18 = 18.0;
   static const double p20 = 20.0;
+  static const double p40 = 40.0;
+  static const double p90 = 90.0;
 }
 
 class AppSize {
@@ -30,6 +34,7 @@ class AppSize {
   static const double s60 = 60.0;
   static const double s68 = 68.0;
   static const double s125 = 125.0;
+  static const double s150 = 150.0;
 }
 
 class DurationConstant {
