41b59825bb7162e7a6c49f9348ff556757828dd9
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-06 01:16:27 +0330

    Connect error view on hole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../presentation/resources/font_manager.dart';
-import '../presentation/resources/color_manager.dart';
+import '../presentation/error/error_view.dart';
 import '../app/my_app.dart';
 
 void main() {
-  ErrorWidget.builder = (FlutterErrorDetails details) => Material(
-        color: ColorManager.error,
-        child: Center(
-          child: Text(
-            details.exception.toString(),
-            style: TextStyle(
-              color: ColorManager.white,
-              fontSize: 18,
-              fontWeight: FontWeightManager.bold,
-            ),
-          ),
-        ),
-      );
+  ErrorWidget.builder = (FlutterErrorDetails errorDetails) =>
+      ErrorView(errorDetails: errorDetails);
   runApp(const MyApp());
 }
