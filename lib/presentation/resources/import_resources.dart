04d8ba477459670e978be75cb21353e5bdedab23
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-06 00:17:47 +0330

    Create import_resources.dart

---
+++ lib/presentation/resources/import_resources.dart
@@ -0,0 +1,8 @@
+export 'assets_manager.dart';
+export 'color_manager.dart';
+export 'font_manager.dart';
+export 'routes_manager.dart';
+export 'strings_manager.dart';
+export 'styles_manager.dart';
+export 'theme_manager.dart';
+export 'values_manager.dart';
