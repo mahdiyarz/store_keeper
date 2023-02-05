73e86b22a3b73caa77a1e464deaaf492615bf210
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-05 22:53:54 +0330

    Delete brands.dart file

--- lib/presentation/import_presentation.dart
+++ lib/presentation/import_presentation.dart
@@ -1,5 +1,4 @@
 export 'incoming_goods_list/incoming_goods_list.dart';
-export 'brands/brands.dart';
 export 'home/home.dart';
 export 'incoming_goods_management/incoming_goods_management.dart';
 export 'brands/brands_management_screen.dart';
