a2b09286c1dcd4c604d73240c2ab3b283bc3565c
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-09 22:26:31 +0330

    Update import_presentation.dart

--- lib/presentation/import_presentation.dart
+++ lib/presentation/import_presentation.dart
@@ -1,4 +1,4 @@
-export 'incoming_goods_list/incoming_goods_list.dart';
+export 'incoming_list/incoming_list_screen.dart';
 export 'home/home.dart';
 export 'incoming_goods_management/incoming_goods_management.dart';
 export 'brands/brands_management_screen.dart';
