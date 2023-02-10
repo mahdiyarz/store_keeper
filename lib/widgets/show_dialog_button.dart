f82ce57c1496aa9d196cdb3d4866847003b279d6
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-11 01:44:27 +0330

    Create show_dialog_button.dart

---
+++ lib/widgets/show_dialog_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../models/import_models.dart';
+import '../presentation/brands/create_or_update_brand_screen.dart';
+
+class ShowDialogButton extends StatelessWidget {
+  const ShowDialogButton({
+    Key? key,
+  }) : super(key: key);
+
+  void _showModalBottomSheet(BuildContext context, {BrandsModel? brandsModel}) {
+    showModalBottomSheet(
+      isDismissible: false,
+      isScrollControlled: true,
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        child: Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: CreateOrUpdateBrandScreen(
+            oldBrand: brandsModel,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        Navigator.of(context).pop();
+        _showModalBottomSheet(context);
+      },
+      icon: const Icon(Icons.edit_note_rounded, size: 18),
+      label: const Text('ایجاد برند جدید'),
+      style: ElevatedButton.styleFrom(
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+    );
+  }
+}
