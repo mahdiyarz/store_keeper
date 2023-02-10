01a2d5c3170800c01d5a095b912739da95b4cee8
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-11 02:37:33 +0330

    Separate incoming list screen to widgets

--- lib/presentation/incoming_list/incoming_list_screen.dart
+++ lib/presentation/incoming_list/incoming_list_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:persian/persian.dart';
 import 'package:store_keeper/bloc/bloc_exports.dart';
 
+import '../../widgets/CircleButton.dart';
+import '../../widgets/incoming_list_list_view.dart';
 import '../../widgets/screens_style.dart';
+import '../../widgets/show_modal_bottom_button.dart';
 import '../resources/import_resources.dart';
 import 'create_or_update_incoming_list_screen.dart';
 
@@ -20,25 +22,18 @@ class IncomingListScreen extends StatelessWidget {
     return ScreensStyle(
       screenTitle: 'ورودی های انبار',
       screenDescription: 'ثبت، اصلاح و تهیه گزارش ورودی های انبار',
-      mainButton: CircleAvatar(
-        backgroundColor: Colors.black12,
-        child: IconButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed(Routes.homeRoute);
-            },
-            icon: const Icon(
-              Icons.arrow_forward,
-              color: Colors.white,
-            )),
+      mainButton: const CircleButton(
+        iconNamedRoute: Routes.homeRoute,
+        iconShape: Icons.arrow_forward,
+        iconColor: Colors.white,
       ),
-      bottomWidget: ElevatedButton.icon(
-        onPressed: () {
-          _showModalBottomSheet(context, width);
-        },
-        icon: const Icon(Icons.post_add_rounded),
-        label: const Text('لیست جدید'),
-        style: ElevatedButton.styleFrom(
-          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      bottomWidget: ShowModalBottomButton(
+        buttonTitle: 'ایجاد برند جدید',
+        buttonIcon: Icons.playlist_add_circle_rounded,
+        showModalChildWidget: CreateOrUpdateIncomingListScreen(
+          boxNumberController: boxNumberController,
+          brandNameController: brandNameController,
+          brandIdController: brandIdController,
         ),
       ),
       screenWidget: BlocBuilder<AppBloc, AppState>(
@@ -48,113 +43,10 @@ class IncomingListScreen extends StatelessWidget {
           }
           if (appState is DisplayAppState) {
             return appState.incomingList.isNotEmpty
-                ? ListView.builder(
-                    physics:
-                        const NeverScrollableScrollPhysics(), //! to disable GridView's scrolling
-                    shrinkWrap: true, //! You won't see infinite size error
-                    itemBuilder: (context, index) {
-                      final brandsList = appState.brandsList;
-
-                      final incomingItemsBrand = brandsList.firstWhere((element) =>
-                          element.brandId ==
-                          appState.incomingList[index]
-                              .brandId); //*This logic find brand of specific arrival goods
-
-                      return Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 3, horizontal: 5),
-                        margin: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: ColorManager.shadow,
-                              blurRadius: 99,
-                            ),
-                          ],
-                        ),
-                        child: PhysicalModel(
-                          color: ColorManager.white,
-                          elevation: 5,
-                          shadowColor: const Color(0xff040039).withOpacity(.2),
-                          borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(8),
-                            bottomRight: Radius.circular(20),
-                            topLeft: Radius.circular(8),
-                            topRight: Radius.circular(20),
-                          ),
-                          child: InkWell(
-                            onTap: () {
-                              // Navigator.of(context).pushNamed(
-                              //   '/arrival-goods-manage',
-                              //   arguments: IncomingGoodsManagementScreen(
-                              //     title: arrivalGoodsBrand.brandName,
-                              //     boxNumber: value
-                              //         .arrivalGoodsItems[index]
-                              //         .numOfBoxes,
-                              //     dateTime: value.arrivalGoodsItems[index]
-                              //         .arrivalGoodsDate,
-                              //   ),
-                              // );
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.all(5),
-                                  margin: const EdgeInsets.all(3),
-                                  alignment: Alignment.center,
-                                  height: width * .15,
-                                  width: width * .15,
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .secondary
-                                        .withOpacity(.3),
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  child: FittedBox(
-                                    child: Text(
-                                      incomingItemsBrand.brandName,
-                                      style: const TextStyle(
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 5, horizontal: 8),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        FittedBox(
-                                          child: Text(
-                                            'ورود ${appState.incomingList[index].numOfBoxes.toString().withPersianNumbers()} جعبه به انبار',
-                                            style:
-                                                const TextStyle(fontSize: 16),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 5),
-                                        FittedBox(
-                                          child: Text(
-                                            'در تاریخ ${appState.incomingList[index].incomingListDate.toPersian().toString()}',
-                                            style: const TextStyle(
-                                                color: Colors.black45),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: appState.incomingList.length,
+                ? IncomingListListView(
+                    width: width,
+                    brandsList: appState.brandsList,
+                    incomingList: appState.incomingList,
                   )
                 :
                 // TODO: Need more design for here
@@ -167,25 +59,5 @@ class IncomingListScreen extends StatelessWidget {
         },
       ),
     );
-  }
-
-  void _showModalBottomSheet(BuildContext context, double width) {
-    showModalBottomSheet(
-        isDismissible: false,
-        isScrollControlled: true,
-        context: context,
-        builder: (context) {
-          return SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: CreateOrUpdateIncomingListScreen(
-                boxNumberController: boxNumberController,
-                brandNameController: brandNameController,
-                brandIdController: brandIdController,
-              ),
-            ),
-          );
-        });
   }
 }
