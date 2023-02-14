369cb8e8c4d72985534d541557db67483a3fdeca
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-15 01:13:12 +0330

    Add 3 images for home buttons

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,7 @@ const String imagePath = 'assets/images';
 
 class ImageAssets {
   static const String notFoundPageRoute = '$imagePath/not_found.png';
+  static const String incomingListScreen = '$imagePath/new_entries.png';
+  static const String incomingGoodsScreen = '$imagePath/incoming_goods.png';
+  static const String countGoodsScreen = '$imagePath/count_goods.png';
 }
