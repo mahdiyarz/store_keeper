8dafd8d91cc2c21bbf5a3d31bd37a63d373c19e4
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-16 22:30:31 +0330

    Add home screen photo

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -5,4 +5,5 @@ class ImageAssets {
   static const String incomingListScreen = '$imagePath/new_entries.png';
   static const String incomingGoodsScreen = '$imagePath/incoming_goods.png';
   static const String countGoodsScreen = '$imagePath/count_goods.png';
+  static const String homeScreen = '$imagePath/home.png';
 }
