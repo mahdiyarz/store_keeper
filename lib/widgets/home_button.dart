6b74f634488eb18cca3d0779155c0bee2ea741db
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-16 22:33:33 +0330

    Change home button style

--- lib/widgets/home_button.dart
+++ lib/widgets/home_button.dart
@@ -6,13 +6,13 @@ class HomeButton extends StatelessWidget {
   const HomeButton({
     Key? key,
     required this.title,
-    required this.imageAssets,
+    required this.icon,
     required this.routeName,
   }) : super(key: key);
 
   final String routeName;
   final String title;
-  final String imageAssets;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
@@ -20,41 +20,42 @@ class HomeButton extends StatelessWidget {
     return InkWell(
       onTap: () => Navigator.of(context).pushReplacementNamed(routeName),
       child: Container(
-        width: width,
-        padding: const EdgeInsets.all(8),
+        width: width / 6,
+        padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: ColorManager.lightGrey,
+              blurRadius: 20,
+            ),
+          ],
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [ColorManager.lightSecondary, ColorManager.secondary],
           ),
           borderRadius: const BorderRadius.all(
-            Radius.circular(45),
+            Radius.circular(20),
           ),
         ),
-        child: Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
-              child: Center(
-                child: Text(
-                  title,
-                  style: Theme.of(context).textTheme.headline3,
+              child: CircleAvatar(
+                backgroundColor: ColorManager.darkPrimary,
+                child: Icon(
+                  icon,
+                  color: ColorManager.white,
                 ),
               ),
             ),
-            Container(
-              width: 100,
-              height: 75,
-              decoration: BoxDecoration(
-                color: ColorManager.white.withOpacity(.6),
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(45),
-                  topLeft: Radius.circular(45),
-                  bottomRight: Radius.circular(5),
-                  topRight: Radius.circular(5),
-                ),
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                title,
+                style: Theme.of(context).textTheme.headline3,
               ),
-              child: Image.asset(imageAssets),
             ),
           ],
         ),
