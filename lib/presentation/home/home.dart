60faaa10a09160798bb0819cf753163e6736e568
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-02-17 09:44:39 +0330

    Extract widgets from home page

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
 
-import '../resources/routes_manager.dart';
+import '../../widgets/control_panel.dart';
+import '../../widgets/green_container.dart';
+import '../resources/import_resources.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushReplacementNamed(Routes.incomingListsRoute);
-                },
-                icon: const Icon(Icons.store_mall_directory_rounded),
-                label: const Text('شمارش ورودی انبار')),
-            ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.storage_rounded),
-                label: const Text('انبار گردانی')),
-            ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.refresh_rounded),
-                label: const Text('بروزرسانی اطلاعات')),
-            ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushReplacementNamed(Routes.brandsRoute);
-                },
-                icon: const Icon(Icons.ballot_rounded),
-                label: const Text('ثبت اطلاعات اولیه')),
-          ],
+      backgroundColor: ColorManager.background,
+      body: SafeArea(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Stack(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 8,
+                    ),
+                    child: GreenContainer(),
+                  ),
+                  ControlPanel(),
+                ],
+              ),
+              Image.asset(ImageAssets.homeScreen),
+              SizedBox(
+                height: width * .001,
+              ),
+            ],
+          ),
         ),
       ),
     );
