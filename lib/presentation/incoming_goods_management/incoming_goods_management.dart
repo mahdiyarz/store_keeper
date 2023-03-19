3b09fde395883dab63bbf84b489ba8b7e59203c6
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-19 21:04:11 +0330

    Implement transferred goods widget

--- lib/presentation/incoming_goods_management/incoming_goods_management.dart
+++ lib/presentation/incoming_goods_management/incoming_goods_management.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:store_keeper/bloc/bloc_exports.dart';
-import 'package:store_keeper/presentation/incoming_goods_management/create_or_update_incoming_goods_screen.dart';
+import 'package:store_keeper/widgets/transferred_goods_list_view.dart';
 
 import '../../models/import_models.dart';
-import '../../widgets/goods_list_view.dart';
 import '../resources/import_resources.dart';
 
 class IncomingGoodsManagementScreen extends StatelessWidget {
@@ -21,38 +20,37 @@ class IncomingGoodsManagementScreen extends StatelessWidget {
       context: context,
       builder: (context) => SingleChildScrollView(
         child: Container(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: CreateOrUpdateIncomingGoodsScreen(
-            brandName: brandName,
-            goodName: goodName,
-            incomingListId: incomingListId,
-          ),
-        ),
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: null
+            // CreateOrUpdateIncomingGoodsScreen(
+            //   brandName: brandName,
+            //   goodName: goodName,
+            //   incomingListId: incomingListId,
+            // ),
+            ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-
     return BlocBuilder<AppBloc, AppState>(
       builder: (context, appState) {
         if (appState is AppStateInitial) {
           context.read<AppBloc>().add(const FetchEvent());
         }
         if (appState is DisplayAppState) {
-          return appState.countGoodsList
-                  .where((element) => element.incomingListId == incomingListId)
+          return appState.countedIncomingsList
+                  .where((element) => element.incomingsId == incomingListId)
                   .isNotEmpty
               ? Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: GoodsListView(
-                    width: width,
+                  child: TransferredGoodsListView(
                     brandsList: appState.brandsList,
                     goodsList: appState.goodsList,
-                    isAdding: false,
+                    warehousesList: appState.warehousesList,
+                    incomingListId: incomingListId,
                   ),
                 )
               : Padding(
